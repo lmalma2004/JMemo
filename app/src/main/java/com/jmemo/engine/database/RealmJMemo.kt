84e374f9306f5fd1fe9690abb8b9cf569a96f19c
lmalma2004 <lmalma2004@naver.com>
2020-03-13 00:06:13 +0900

    기기 호환성 수정

--- app/src/main/java/com/jmemo/engine/database/RealmJMemo.kt
+++ app/src/main/java/com/jmemo/engine/database/RealmJMemo.kt
@@ -2,10 +2,14 @@ package com.jmemo.engine.database
 
 import android.app.Application
 import io.realm.Realm
+import io.realm.RealmConfiguration
 
 class RealmJMemo : Application(){
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
+        //val config = RealmConfiguration.Builder()
+        //    .deleteRealmIfMigrationNeeded()
+        //    .build()
     }
 }
